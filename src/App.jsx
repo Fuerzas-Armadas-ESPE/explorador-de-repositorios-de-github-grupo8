4cf5af877d4f067db91cce96be886301a4ef607e
jacortez3 <jacortez3@espe.edu.ec>
2024-02-25 19:02:07 -0500

    Pagination and information of user Git

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import {
   Container,
   AppBar,
   Toolbar,
+  CssBaseline,
+  createTheme,
+  ThemeProvider,
+  Avatar,
 } from "@mui/material";
 import RepoList from "./components/RepoList";
 
@@ -22,32 +26,51 @@ function App() {
     setShowRepoList(true);
   };
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
   return (
-    <div>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mi Explorador de Repositorios
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Container maxWidth="sm" sx={{ marginTop: "20px" }}>
-        <form onSubmit={handleFormSubmit}>
-          <TextField
-            label="Nombre de Usuario"
-            variant="outlined"
-            value={username}
-            onChange={handleUsernameChange}
-            fullWidth
-            sx={{ marginBottom: "10px" }}
-          />
-          <Button type="submit" variant="contained" fullWidth>
-            Buscar Repositorios
-          </Button>
-        </form>
-        {showRepoList && <RepoList username={username} />}
-      </Container>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Mi Explorador de Repositorios
+            </Typography>
+            
+            <Avatar alt="User Avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO2L2WFUXd-4pnJoprAtrMCu17xm1OUoInjrgGYxAf9VsumiYdh6-4NjjPEp6s_KEzRsc&usqp=CAU" />
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm" sx={{ marginTop: "20px" }}>
+          <br></br>
+          <form onSubmit={handleFormSubmit}>
+            <TextField
+              label="Nombre de Usuario"
+              variant="standard"
+              value={username}
+              onChange={handleUsernameChange}
+              fullWidth
+              sx={{ marginBottom: "10px" }}
+            />
+            <Button type="submit" variant="contained" color="success" fullWidth>
+              Buscar Repositorios
+            </Button>
+          </form>
+          
+        </Container>
+      </div>
+      <div>
+        <Container sx={{ marginTop: "20px" }}>
+          {showRepoList && <RepoList username={username} />}
+        </Container>
+
+      </div>
+
+    </ThemeProvider>
   );
 }
 
--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,36 +1,79 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import {
+  Avatar,
+  CircularProgress,
+  Pagination
+} from "@mui/material";
 
 const RepoList = ({ username }) => {
   const [repos, setRepos] = useState([]);
+  const [user, setUser] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reposPerPage] = useState(5);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(
+        const userResponse = await axios.get(
+          `https://api.github.com/users/${username}`
+        );
+        setUser(userResponse.data);
+
+        const reposResponse = await axios.get(
           `https://api.github.com/users/${username}/repos`
         );
-        const sortedRepos = response.data.sort((a, b) => b.size - a.size);
+        const sortedRepos = reposResponse.data.sort(
+          (a, b) => new Date(b.created_at) - new Date(a.created_at)
+        );
         setRepos(sortedRepos);
       } catch (error) {
-        console.error("Error fetching repos:", error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
   }, [username]);
 
+  if (!user) {
+    return <div> <CircularProgress /></div>;
+  }
+
+  // Pagination
+  const indexOfLastRepo = currentPage * reposPerPage;
+  const indexOfFirstRepo = indexOfLastRepo - reposPerPage;
+  const currentRepos = repos.slice(indexOfFirstRepo, indexOfLastRepo);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <div>
-      <h2>Todos los repositorios de {username}</h2>
+      <h2>Todos los repositorios de {user.name}</h2>     
+      <p>Username: {user.login}</p>
+      <p>Followers: {user.followers}</p>
+      <p>Public Repos: {user.public_repos}</p>
+      <p>URL: {user.html_url}</p>
+      <Avatar alt="User Avatar" src={user.avatar_url} />
+      <br />
+    
       <ul>
-        {repos.map((repo) => (
+        {currentRepos.map((repo) => (
           <li key={repo.id}>
             {repo.name} - Tamaño: {repo.size}
           </li>
         ))}
       </ul>
+
+      <div>
+        <Pagination
+          count={Math.ceil(repos.length / reposPerPage)}
+          page={currentPage}
+          onChange={handlePageChange}
+        />
+      </div>
     </div>
   );
 };
